be279e2e2c21f20b17c8b9595b4f428a4462d50a
A Fakhrurozi S <haifahrul@gmail.com>
2018-12-16 01:20:52 +0700

    add title

--- app/modules/admin/views/jadwal-wawancara/create.php
+++ app/modules/admin/views/jadwal-wawancara/create.php
@@ -12,7 +12,7 @@ $this->params['title'] = $this->title;
 ?>
 <div class="jadwal-wawancara-create">
 
-    <h1><?php Html::encode($this->title) ?></h1>
+    <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
--- app/modules/admin/views/jadwal-wawancara/update.php
+++ app/modules/admin/views/jadwal-wawancara/update.php
@@ -5,19 +5,21 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\JadwalWawancara */
 
-$this->title = Yii::t('app', 'Update {modelClass}: ', [
-    'modelClass' => 'Jadwal Wawancara',
-]) . ' ' . $model->id;
+$this->title = Yii::t('app', 'Update {modelClass} ', [
+            'modelClass' => 'Jadwal Wawancara',
+        ]);
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Jadwal Wawancaras'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 ?>
 <div class="jadwal-wawancara-update">
 
-    <h1><?php //Html::encode($this->title) ?></h1>
+    <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
+    <?=
+    $this->render('_form', [
         'model' => $model,
-    ]) ?>
+    ])
+    ?>
 
 </div>
