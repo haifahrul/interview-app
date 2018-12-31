f039f728da6e9aed3af262d58ded81adbc8d72f1
haifahrul <haifahrul@gmail.com>
2019-01-01 00:16:01 +0700

    Sort ID DESC

--- app/models/search/UserCalonSearch.php
+++ app/models/search/UserCalonSearch.php
@@ -10,12 +10,11 @@ use app\models\UserCalon;
 /**
  * UserCalonSearch represents the model behind the search form about `app\models\UserCalon`.
  */
-class UserCalonSearch extends UserCalon
-{
+class UserCalonSearch extends UserCalon {
 
     public $page;
-    public function rules()
-    {
+
+    public function rules() {
         return [
             [['id', 'user_id', 'usia', 'keputusan_id', 'status'], 'integer'],
             [['nama_calon', 'pendidikan', 'jabatan_yang_dilamar', 'phone', 'email'], 'safe'],
@@ -23,26 +22,23 @@ class UserCalonSearch extends UserCalon
         ];
     }
 
-
-    public function scenarios()
-    {
+    public function scenarios() {
         // bypass scenarios() implementation in the parent class
         return Model::scenarios();
     }
 
-    public function search($params)
-    {
+    public function search($params) {
         $query = UserCalon::find()->asArray();
         $query->joinWith(['keputusan', 'jabatan']);
-        $query->orderBy('id DESC');
+        $query->orderBy(['user_calon.id' => SORT_DESC]);
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
         ]);
 
         $this->load($params);
-        if(isset($this->page)){
-            $dataProvider->pagination->pageSize=$this->page; 
+        if (isset($this->page)) {
+            $dataProvider->pagination->pageSize = $this->page;
         }
 
         $query->andFilterWhere([
@@ -54,16 +50,15 @@ class UserCalonSearch extends UserCalon
         ]);
 
         $query->andFilterWhere(['like', 'nama_calon', $this->nama_calon])
-            ->andFilterWhere(['like', 'pendidikan', $this->pendidikan])
-            ->andFilterWhere(['like', 'jabatan.nama', $this->jabatan_yang_dilamar])
-            ->andFilterWhere(['like', 'phone', $this->phone])
-            ->andFilterWhere(['like', 'email', $this->email]);
+                ->andFilterWhere(['like', 'pendidikan', $this->pendidikan])
+                ->andFilterWhere(['like', 'jabatan.nama', $this->jabatan_yang_dilamar])
+                ->andFilterWhere(['like', 'phone', $this->phone])
+                ->andFilterWhere(['like', 'email', $this->email]);
 
         return $dataProvider;
     }
 
-    public function searchBeranda($params)
-    {
+    public function searchBeranda($params) {
         $query = UserCalon::find()->asArray();
         $query->joinWith('keputusan');
         $query->where(['status' => 2]);
@@ -73,8 +68,8 @@ class UserCalonSearch extends UserCalon
         ]);
 
         $this->load($params);
-        if(isset($this->page)){
-            $dataProvider->pagination->pageSize=$this->page; 
+        if (isset($this->page)) {
+            $dataProvider->pagination->pageSize = $this->page;
         }
 
         $query->andFilterWhere([
@@ -86,11 +81,12 @@ class UserCalonSearch extends UserCalon
         ]);
 
         $query->andFilterWhere(['like', 'nama_calon', $this->nama_calon])
-            ->andFilterWhere(['like', 'pendidikan', $this->pendidikan])
-            ->andFilterWhere(['like', 'jabatan_yang_dilamar', $this->jabatan_yang_dilamar])
-            ->andFilterWhere(['like', 'phone', $this->phone])
-            ->andFilterWhere(['like', 'email', $this->email]);
+                ->andFilterWhere(['like', 'pendidikan', $this->pendidikan])
+                ->andFilterWhere(['like', 'jabatan_yang_dilamar', $this->jabatan_yang_dilamar])
+                ->andFilterWhere(['like', 'phone', $this->phone])
+                ->andFilterWhere(['like', 'email', $this->email]);
 
         return $dataProvider;
     }
+
 }
